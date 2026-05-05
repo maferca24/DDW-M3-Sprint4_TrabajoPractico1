67f3b35897aa44602f241c0c5f70ef426e7162b7
maferca24 <mafercarrenio@gmail.com>
2026-05-05 13:00:15 -0300

    modifique archivo de servidor

--- app.mjs
+++ app.mjs
@@ -3,14 +3,28 @@ import {connectDB} from './config/dbConfig.mjs';
 import superHeroRoutes from './routes/superHeroesRoutes.mjs';
 import heroesroutefront from './routes/superHeroFront.mjs'
 
+//Crear instancia de Express
 const app = express();
+//Definir el puerto para el servidor, usando una variable de entorno o el puerto 3000 por defecto
 const PORT = process.env.PORT||3000;
+
+//Configurar ejs como motor de plantillas
+app.set("view engine","ejs");
+app.set("views", path.resolve("./views")); //Especificar la carpeta donde se encuentran las vistas (plantillas ejs)
+
+//Configurar express-ejs-layouts para usar un layout común en las vistas
+import expressLayouts from 'express-ejs-layouts';
+app.use(expressLayouts);
+app.set('layout','layout'); //Especificar la ubicación del layout principal
+
+//Servir archivos estáticos desde la carpeta "public" (para CSS, JS, imágenes, etc.)
+import path from 'path';
+app.use(express.static(path.resolve("./public")));
 
 //Middleware para parsear JSON (Mid para que las solicitudes se conviertan a JSON automáticamente)
 app.use (express.json());
 
-//Configurar ejs como motor de plantillas
-app.set("view engine","ejs");
+
 
 //Conexión a MongoDB
 connectDB();
