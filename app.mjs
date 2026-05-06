baf4b5533ab5033de99f2392ec057c53f48232db
maferca24 <mafercarrenio@gmail.com>
2026-05-06 11:47:37 -0300

    modifique archivo navbar

--- app.mjs
+++ app.mjs
@@ -15,7 +15,7 @@ app.set("views", path.resolve("./views")); //Especificar la carpeta donde se enc
 //Configurar express-ejs-layouts para usar un layout común en las vistas
 import expressLayouts from 'express-ejs-layouts';
 app.use(expressLayouts);
-app.set('layout','layout'); //Especificar la ubicación del layout principal
+app.set('layout','layout'); //Especificar la ubicación del layout principal-Indica que use layout.ejs por defecto para todas las vistas, a menos que se especifique lo contrario en la ruta correspondiente.
 
 //Servir archivos estáticos desde la carpeta "public" (para CSS, JS, imágenes, etc.)
 import path from 'path';
